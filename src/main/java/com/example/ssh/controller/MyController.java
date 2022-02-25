eaa6c0a7fcf46568f341c93d21d70464eb92eb80
rahmonali96 <rahmonaliyoqubov@gmail.com>
2022-02-25 16:26:49 +0500

    test for auto deploy-13

--- src/main/java/com/example/ssh/controller/MyController.java
+++ src/main/java/com/example/ssh/controller/MyController.java
@@ -1,24 +1,13 @@
 package com.example.ssh.controller;
 
-import com.example.ssh.service.MyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-
 @RestController
 public class MyController {
-    private final MyService myService;
-
-    @Autowired
-    public MyController(MyService myService) {
-        this.myService = myService;
-    }
 
     @GetMapping
-    public String test() throws IOException {
-        return myService.test();
+    public String test() {
+        return "000";
     }
 }
