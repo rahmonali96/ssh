330fa01fbcf69d4768845356058c4890a5b95dd3
rahmonali96 <rahmonaliyoqubov@gmail.com>
2022-02-25 15:41:59 +0500

    test for auto deploy-6

--- src/main/java/com/example/ssh/controller/MyController.java
+++ src/main/java/com/example/ssh/controller/MyController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
     @GetMapping
     public String test() {
-        return "auto deploy";
+        return "auto deploy changed";
     }
 }
