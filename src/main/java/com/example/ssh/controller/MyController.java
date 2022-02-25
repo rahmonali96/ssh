78f7df2f830c9d8ee76bdc2a4d2f040e9cec3704
rahmonali96 <rahmonaliyoqubov@gmail.com>
2022-02-25 16:15:43 +0500

    test for auto deploy-10

--- src/main/java/com/example/ssh/controller/MyController.java
+++ src/main/java/com/example/ssh/controller/MyController.java
@@ -2,25 +2,11 @@ package com.example.ssh.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @Controller
 public class MyController {
     @GetMapping
     public String test() {
         return "index";
-    }
-
-    @GetMapping("/pwd")
-    @ResponseBody
-    public String pwd(ProcessBuilder processBuilder) {
-        try {
-            return new String(processBuilder.command("${pwd}").start().getInputStream().readAllBytes());
-        } catch (IOException e) {
-            return "exception";
-        }
     }
 }
