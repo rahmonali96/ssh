234df51684bbbd69d5369dcd561ad791e5094219
rahmonali96 <rahmonaliyoqubov@gmail.com>
2022-02-25 15:50:24 +0500

    test for auto deploy-7

--- src/main/java/com/example/ssh/controller/MyController.java
+++ src/main/java/com/example/ssh/controller/MyController.java
@@ -1,12 +1,13 @@
 package com.example.ssh.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MyController {
     @GetMapping
     public String test() {
-        return "auto deploy changed";
+        return "index";
     }
 }
