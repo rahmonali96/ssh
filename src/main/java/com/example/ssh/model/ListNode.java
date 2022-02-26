9a7dc2b376751062f05a30d3be317aa198f11453
rahmonali96 <rahmonaliyoqubov@gmail.com>
2022-02-26 11:33:35 +0500

    26.02.2022-2

--- src/main/java/com/example/ssh/model/ListNode.java
+++ src/main/java/com/example/ssh/model/ListNode.java
@@ -1,16 +1,21 @@
 package com.example.ssh.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class ListNode {
     public int val;
     public ListNode next;
 
+    @JsonCreator
     public ListNode() {
     }
 
+    @JsonCreator
     public ListNode(int val) {
         this.val = val;
     }
 
+    @JsonCreator
     public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
